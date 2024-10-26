96c7f8a15cf9127c97c595f4ce17082fac66fbc1
pinotalexandre <hialexpinot@gmail.com>
2024-10-26 18:37:13 +0200

    blog

--- build.js
+++ build.js
@@ -45,7 +45,7 @@ async function build() {
           <meta charset="UTF-8">
           <meta name="viewport" content="width=device-width, initial-scale=1.0">
           <title>${metadata.title}</title>
-          <link rel="stylesheet" href="/styles.css">
+          <link rel="stylesheet" href="../styles.css">
         </head>
         <body>
           <article>
